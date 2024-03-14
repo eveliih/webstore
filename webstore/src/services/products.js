279cd47768535633a860f0c97856e5db38327c6f
Eveliina <eveliina.heino@metropolia.fi>
2024-03-14 21:54:38 +0200

    use environmental variable for services base url

--- webstore/src/services/products.js
+++ webstore/src/services/products.js
@@ -1,11 +1,10 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/fruits'
+import axios from 'axios';
 
+const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
 }
 
-
-export default { getAll }
+export default { getAll };
