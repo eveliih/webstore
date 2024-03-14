491ddbea3389afac9596187032012cced4608894
Eveliina <eveliina.heino@metropolia.fi>
2024-03-14 20:42:17 +0200

    update gitignore and other stuff for azure deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
-
+app.use(express.static('dist'))
 
 let fruits = [
   {
@@ -50,7 +50,7 @@ app.get('/api/fruits', (request, response) => {
   response.json(fruits)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
