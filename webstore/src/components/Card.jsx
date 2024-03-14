d0e4445768872c44f199fe0239e6d9e6a03858e0
Eveliina <eveliina.heino@metropolia.fi>
2024-03-14 15:39:18 +0200

    add card hover effect and on click

--- webstore/src/components/Card.jsx
+++ webstore/src/components/Card.jsx
@@ -9,12 +9,24 @@ CardComponent.propTypes = {
   price: PropTypes.string.isRequired,
 };
 
-
 function CardComponent(props) {
   const { imageUrl, title, price } = props;
 
+  const handleCardClick = () => {
+    console.log('Card clicked');
+  };
+
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card
+      style={{ width: '18rem', transition: 'transform 0.3s', transform: 'scale(1)', boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)' }}
+      onClick={handleCardClick}
+      onMouseEnter={(e) => {
+        e.currentTarget.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.boxShadow = '0 0 5px rgba(0, 0, 0, 0.3)';
+      }}
+    >
       <Card.Img variant="top" src={imageUrl} style={{ width: '18rem', height: '18rem' }} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
--- webstore/tuntikirjanpito.md
+++ webstore/tuntikirjanpito.md
@@ -5,5 +5,7 @@
 | 11.3 | 2    | Projektin repon luominen, projektin suunnittelu |
 | 11.3 | 2    | Azureen deploy workflown luominen ja testaus |
 | 11.3 | 4    | React boostrapin lisäys, navbar lisäys ja kokeilut |
+| 14.3 | 1    | Card komponentin lisäys ja kuvien kokeilu |
+
 
 
