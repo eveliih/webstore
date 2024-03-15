d08210ff7d04887fecff0d5334f044946b32db7c
Eveliina <eveliina.heino@metropolia.fi>
2024-03-15 16:34:02 +0200

    add test connection to database

--- index.js
+++ index.js
@@ -54,3 +54,27 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+require('dotenv').config()
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mssql',
+  dialectOptions: {
+    options: {
+      encrypt: true, 
+    },
+  },
+});
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+main()
