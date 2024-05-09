46240a86ecf6ff03ffc9b859ce23e6a39ff67843
Eveliina <eveliina.heino@metropolia.fi>
2024-05-09 17:59:28 +0300

    style notification

--- webstore/src/components/Notification.jsx
+++ webstore/src/components/Notification.jsx
@@ -1,23 +1,19 @@
 import { useSelector } from 'react-redux'
+import Alert from 'react-bootstrap/Alert'
 
 const Notification = () => {
   const info = useSelector(({ notification }) => notification)
-  if ( !info.message) {
-    return 
+  if (!info.message) {
+    return null
   }
 
+  const variant = info.type === 'error' ? 'danger' : 'success'
 
-  const style = {
-    color: info.type === 'error' ? 'red' : 'green',
-    background: 'lightgrey',
-    fontSize: 20,
-    borderStyle: 'solid',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  }
-
-  return <div style={style}>{info.message}</div>
+  return (
+    <Alert variant={variant} className='notify'>
+      {info.message}
+    </Alert>
+  )
 }
 
 export default Notification
