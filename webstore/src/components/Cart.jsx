434c6cad54540be391a3b937ba71cb57588d6833
Eveliina <eveliina.heino@metropolia.fi>
2024-08-28 11:21:48 +0300

    get price and name for cartItems from the products

--- webstore/src/components/Cart.jsx
+++ webstore/src/components/Cart.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Cart = () => {
-  // Access cart items from the Redux state
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const products = useSelector((state) => state.products);
+  console.log(cartItems);
 
   return (
     <div>
       <h1>Your Shopping Cart</h1>
       {cartItems.length > 0 ? (
         <ul>
-          {cartItems.map((item, index) => (
-            <li key={index}>
-              {item.name} - {item.quantity} x ${item.price}
-            </li>
-          ))}
+          {cartItems.map((item, index) => {
+            const product = products.find(
+              (product) => product.id === item.product_id
+            );
+            return (
+              <li key={index}>
+                {product.name} - {item.quantity} x ${product.price}
+              </li>
+            );
+          })}
         </ul>
       ) : (
         <p>Your cart is empty.</p>
