fc5f1bdb0d2f5fb0535293840d50c89883d365a4
Eveliina <eveliina.heino@metropolia.fi>
2024-10-03 21:37:21 +0300

    try fix workflow

--- util/config.js
+++ util/config.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 3001;
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
+const SECRET = process.env.SECRET;
+
 module.exports = {
   DATABASE_URL,
   PORT,
+  SECRET,
 };
