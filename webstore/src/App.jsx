d6d4613798b883430b6069c8ce7ec6b4d5572b98
Eveliina <eveliina.heino@metropolia.fi>
2024-10-04 16:15:35 +0300

    add basic footer

--- webstore/src/App.jsx
+++ webstore/src/App.jsx
@@ -1,41 +1,37 @@
-import './App.css'
-import NavBar from './components/NavBar'
-import Container from 'react-bootstrap/Container';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect} from 'react';
-import ProductsBreadcrumb from './components/ProductsBreadcrumb';
-import RoutesComponent from './components/RoutesComponent';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import ProductsBreadcrumb from "./components/ProductsBreadcrumb";
+import RoutesComponent from "./components/RoutesComponent";
 import { useDispatch } from "react-redux";
 import { initializeProducts } from "./reducers/productReducer";
- import {
-  BrowserRouter as Router,
-} from 'react-router-dom'
- 
-
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   const dispatch = useDispatch();
- 
+
   useEffect(() => {
     dispatch(initializeProducts());
   }, [dispatch]);
 
   return (
     <>
-    <div className='top-bar'></div>
-    <Router>  
-    <NavBar />
-    <Container id='custom-container'>
-       <ProductsBreadcrumb  />
-    </Container>
-      <Container id='appContainer' >
-          <RoutesComponent ></RoutesComponent>
-      </Container>
-    </Router>
-
+      <div className="top-bar"></div>
+      <Router>
+        <NavBar />
+        <Container id="custom-container">
+          <ProductsBreadcrumb />
+        </Container>
+        <Container id="appContainer">
+          <RoutesComponent></RoutesComponent>
+        </Container>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
